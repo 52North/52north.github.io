37da13c82e0a96854ef34ac9e4eebc5e9d30d4d2
Jan Schulte <j.schulte@52north.org>
2016-11-21 09:46:07 +0100

    cleanup vendor.browser.ts

--- src/vendor.browser.ts
+++ src/vendor.browser.ts
@@ -6,31 +6,3 @@
 // TODO(gdi2290): switch to DLLs
 
 // Angular 2
-import '@angular/platform-browser';
-import '@angular/platform-browser-dynamic';
-import '@angular/core';
-import '@angular/common';
-import '@angular/forms';
-import '@angular/http';
-import '@angular/router';
-
-// Hammer.js
-// We need to import this library in order for Material to work
-// Material needs this for md-tooltips
-import 'hammerjs/hammer.js';
-
-// AngularClass
-import '@angularclass/hmr';
-
-// RxJS
-import 'rxjs/add/operator/map';
-import 'rxjs/add/operator/mergeMap';
-
-if ('production' === ENV) {
-  // Production
-
-
-} else {
-  // Development
-
-}
