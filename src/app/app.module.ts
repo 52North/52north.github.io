03ebcbff0544461ced528a664d030d19ecb3cc62
Jan Schulte <j.schulte@52north.org>
2018-03-14 08:40:00 +0100

    update versions

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,7 @@ import { NgModule, ApplicationRef } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
-import { MaterialModule } from '@angular/material';
+import { MatCardModule } from '@angular/material';
 import { RouterModule } from '@angular/router';
 import { removeNgStyles, createNewHosts, createInputTransfer } from '@angularclass/hmr';
 
@@ -45,7 +45,7 @@ type StoreType = {
         BrowserModule,
         FormsModule,
         HttpModule,
-        MaterialModule,
+        MatCardModule,
         RouterModule.forRoot(ROUTES, { useHash: true })
     ],
     providers: [ // expose our Services and Providers into Angular's dependency injection
